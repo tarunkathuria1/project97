b3aff5fa06be24fcade93732799e808d314b78bf
tarunkathuria1 <72028064+tarunkathuria1@users.noreply.github.com>
2021-08-21 17:26:34 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,19 @@
+from typing import Generator
+
+
+def gauessingGame():
+    fileName =  input("guessing game ")
+    numberOfGaessing=0
+
+     file=open(fileName,'r')
+    for line in file:
+        world=line.split()
+        guessANumber(between 1 and 9):
+        
+        while chances<5:
+            if guess == number:
+                print('congratulation you won!!!')
+                break
+            if not change<5:
+                print(you lose!!! the number is number)
+
